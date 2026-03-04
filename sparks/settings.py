8abd82ad08d268e73a2206df953c6f1d24e99500
Shivakumar <shivakumarkarnati005@gmail.com>
2026-03-04 19:00:56 +0530

    change database in setting

--- sparks/settings.py
+++ sparks/settings.py
@@ -83,11 +83,12 @@ WSGI_APPLICATION = 'sparks.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
 }
 
 
