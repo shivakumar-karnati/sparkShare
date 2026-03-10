b40dbc85927d1e9f3a3f0efc801a86b52e8e37aa
Shivakumar <shivakumarkarnati005@gmail.com>
2026-03-10 18:52:24 +0530

    added postgresql database

--- sparks/settings.py
+++ sparks/settings.py
@@ -83,11 +83,15 @@ WSGI_APPLICATION = 'sparks.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
